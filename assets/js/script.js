b3fc30f2b7ee48858780e5ceec4c8a8bb37645bd
Przemysław Wróbel <p.wrobel@adhdinteractive.pl>
2025-01-05 17:15:17 +0100

    finished solution

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+const buttons = document.querySelectorAll(".faq-question button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const faqItem = button.closest("li");
+        const answer = faqItem.querySelector(".faq-answer");
+
+        closeOtherItems(faqItem);
+        toggleActiveState(faqItem, button, answer);
+    });
+});
+
+function closeOtherItems(faqItem) {
+    const allFaqItems = document.querySelectorAll(".faq-list-item");
+    allFaqItems.forEach((item) => {
+        if (item !== faqItem) {
+            const otherAnswer = item.querySelector(".faq-answer");
+            const otherButton = item.querySelector("button");
+            item.classList.remove("active");
+            otherAnswer.style.display = "none";
+            otherButton
+                .querySelector("img")
+                .setAttribute("src", "/assets/images/icon-plus.svg");
+            otherButton.setAttribute("aria-expanded", "false");
+        }
+    });
+}
+
+function toggleActiveState(faqItem, button, answer) {
+    const isActive = faqItem.classList.contains("active");
+    faqItem.classList.toggle("active");
+    button.setAttribute("aria-expanded", !isActive);
+
+    if (isActive) {
+        answer.style.display = "none";
+        button
+            .querySelector("img")
+            .setAttribute("src", "/assets/images/icon-plus.svg");
+    } else {
+        answer.style.display = "block";
+        button
+            .querySelector("img")
+            .setAttribute("src", "/assets/images/icon-minus.svg");
+    }
+}
